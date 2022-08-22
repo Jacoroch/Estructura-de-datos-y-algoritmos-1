6fe2940035e0346ae36a52df965bd77f1d29874c
Jacoroch <110855576+Jacoroch@users.noreply.github.com>
2022-08-21 20:10:16 -0500

    Delete Fibonacci.py

--- Fibonacci.py
+++
@@ -1,8 +0,0 @@
-def fibonacci(n):
-    if n == 1 or  n == 0:
-        return 1
-    else:
-        return fibonacci(n-1) + fibonacci(n-2)
-
-num = int(input("Ingrese el n fibonacci por conocer: "))
-print(fibonacci(num))
